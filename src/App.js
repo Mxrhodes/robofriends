097dc147d847225c321bf367ed4e96adc289c602
Mateo Rhodes <rhodesmateo@gmail.com>
2023-04-06 22:11:47 -0400

    Fetching json data for users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 import './App.css';
 
 
@@ -10,9 +9,16 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots:users }))
+        
     }
 
     onSearchChange = (event) => {
@@ -23,19 +29,26 @@ class App extends Component {
 
     render() {
         const filteredRobots = this.state.robots.filter( robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()) // This grabs all robots
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()) 
+            // This grabs all robots
             // But when "searchfield" is modified -> filtered robot looks for the given name
         })
-        return (
-            // SearchBox -- You can create methods that modify the "this.states" that we supply.
-            // CardList -- We first supplied it with "all robots" - now we supply it with filtered robots
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} /> 
-            </div>
-        
-        )
+        if(this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+            return (
+                // SearchBox -- You can create methods that modify the "this.states" that we supply.
+                // CardList -- We first supplied it with "all robots" - now we supply it with filtered robots
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} /> 
+                    </Scroll>
+                </div>
+            
+            )
+        }
     }
 }
     
